f12ebbeb1b0cf0e7704f31538e65f37face9103a
cleancode7 <57779244+cleancode7@users.noreply.github.com>
2025-07-11 08:58:53 +0000

    copy from googlekorea.org

--- README.md
+++
@@ -1,1 +0,0 @@
-# cleancode7.github.io
--- script.js
+++
@@ -1,9 +0,0 @@
-// Responsive navigation menu
-const hamburger = document.querySelector('.hamburger');
-const navMenu = document.querySelector('.nav-menu');
-
-if (hamburger && navMenu) {
-    hamburger.addEventListener('click', () => {
-        navMenu.classList.toggle('active');
-    });
-} 
